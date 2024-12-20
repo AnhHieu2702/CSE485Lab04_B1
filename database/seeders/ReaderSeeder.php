c83c6b6f263cec564d66cbda465442ed03a7e458
Anhhieu2702 <vungochieu0999@gmail.com>
2024-12-20 10:51:03 +0700

    Create seeders and define properties, use Faker to add data

--- database/seeders/ReaderSeeder.php
+++ database/seeders/ReaderSeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
+use App\Models\Reader;
 class ReaderSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
+
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        for ($i = 0; $i < 15; $i++) {
+            $reader = Reader::create([
+                'name' => $faker->name,
+                'birthday' => $faker->date(),
+                'address' =>  $faker->address,
+                'phone' => $faker->phoneNumber,
+            ]);
+        }
     }
 }
