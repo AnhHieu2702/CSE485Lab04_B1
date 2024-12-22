dec486db96e5872a1794f195784a457ec596e775
td-29th02 <tuandung290204@gmail.com>
2024-12-22 21:48:46 +0700

    create show_view and comlete show_function

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,1 +1,30 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Chi tiết sách</h1>
+        <table class="table table-bordered">
+            <tr>
+                <th>Tên sách</th>
+                <td>{{ $book->name }}</td>
+            </tr>
+            <tr>
+                <th>Tác giả</th>
+                <td>{{ $book->author }}</td>
+            </tr>
+            <tr>
+                <th>Thể loại</th>
+                <td>{{ $book->category }}</td>
+            </tr>
+            <tr>
+                <th>Năm xuất bản</th>
+                <td>{{ $book->year }}</td>
+            </tr>
+            <tr>
+                <th>Số lượng</th>
+                <td>{{ $book->quantity }}</td>
+            </tr>
+        </table>
+        <a href="{{ route('books.index') }}" class="btn btn-secondary">Quay lại</a>
+    </div>
+@endsection
